246ad04b2f4581f9e1881f1e8ee8a7651b0ddabe
Stef <paulkelele@gmail.com>
2024-01-02 17:21:11 +0100

    Init

---
+++ src/main/java/com/example/wg/WgApplication.java
@@ -0,0 +1,13 @@
+package com.example.wg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/wg/web/RestApiController.java
@@ -0,0 +1,7 @@
+package com.example.wg.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiController {
+}
